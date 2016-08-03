e71a240556ae89179318e2f7aeedac17c82ffd74
ie2030 <ielongford@gmail.com>
2016-08-03 21:28:05 +0100

    Client asked me to change a comment.
    The client asked for the comment to be added. I made that
    change for them.
    
    *Client asked for comment change
    *I don`t see this effective anything
    
    Closes-bug: #215654

--- class.rb
+++ class.rb
@@ -1,3 +1,5 @@
-class Class 
 
-attr_accessor 
+my_arr = %w[ruby javascript java php c++ python assembler jquery]
+long_wrd = ''
+my_arr.each {|wrb| long_wrd = wrb if long_wrd.length < wrb.length }
+puts long_wrd
