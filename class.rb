d974c5466f4a67688b0703fa90e80c62fc767d97
ie2030 <ielongford@gmail.com>
2016-08-03 21:36:02 +0100

    Added new comment

--- class.rb
+++ class.rb
@@ -3,3 +3,6 @@ my_arr = %w[ruby javascript java php c++ python assembler jquery]
 long_wrd = ''
 my_arr.each {|wrb| long_wrd = wrb if long_wrd.length < wrb.length }
 puts long_wrd
+
+
+#Here is comment
